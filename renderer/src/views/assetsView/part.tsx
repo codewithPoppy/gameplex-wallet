0ac3e65dd9585fd7da460eb304386093532d6ae7
BrainStorm0417 <azminohabdullahdev@gmail.com>
2022-07-05 02:46:04 -0600

    fix line border & color

--- renderer/src/views/assetsView/part.tsx
+++ renderer/src/views/assetsView/part.tsx
@@ -11,9 +11,9 @@ export default function Part(props) {
                         <p className="text-lg text-[#D099FF]">{props.value}</p>
                     </div>
                     <div className="absolute flex w-[40%] h-[30px] top-1/2 left-1/2">
-                        <div className={props.index < 5 ? "border-b-[1px] border-[#fff] grow-1 flex-grow" : "border-t-[1px] border-[#fff] grow-1 flex-grow"}></div>
-                        <div className={props.index < 5 ? "border-l-[1px] border-white w-[30px]" : "border-l-[1px] border-white w-[30px]"} style={props.index < 5 ? {transform: "skewX(-45deg)", marginLeft: '15px',marginRight: '-15px'} : {transform: "skewX(45deg)", marginLeft: '15px',marginRight: '-15px'}}></div>
-                        <div className={props.index < 5 ? "border-t-[1px] border-[#fff] grow-1 flex-grow" : "border-b-[1px] border-[#fff] grow-1 flex-grow"}></div>
+                        <div className={props.index < 5 ? "border-b-[1px] border-[#FFF7] grow-1 flex-grow" : "border-t-[1px] border-[#fff7] grow-1 flex-grow"}></div>
+                        <div className={props.index < 5 ? "border-l-[1px] border-[#FFF7] w-[30px]" : "border-l-[1px] border-[#FFF7] w-[30px]"} style={props.index < 5 ? {transform: "skewX(-45deg)", marginLeft: '15px',marginRight: '-15px'} : {transform: "skewX(45deg)", marginLeft: '15px',marginRight: '-15px'}}></div>
+                        <div className={props.index < 5 ? "border-t-[1px] border-[#FFF7] grow-1 flex-grow" : "border-b-[1px] border-[#FFF7] grow-1 flex-grow"}></div>
                     </div>
                 </div>
             );
@@ -27,9 +27,9 @@ export default function Part(props) {
                         <p className="text-lg text-[#D099FF]">{props.value}</p>
                     </div>
                     <div className="absolute flex w-1/2 h-[30px] top-1/2 right-1/3">
-                    <div className={props.index < 5 ? "border-t-[1px] border-[#fff] grow-1 flex-grow" : "border-b-[1px] border-[#fff] grow-1 flex-grow"}></div>
-                        <div className={props.index < 5 ? "border-l-[1px] border-white w-[30px]" : "border-l-[1px] border-white w-[30px]"} style={props.index < 5 ? {transform: "skewX(45deg)", marginLeft: '15px',marginRight: '-15px'} : {transform: "skewX(-45deg)", marginLeft: '15px',marginRight: '-15px'}}></div>
-                        <div className={props.index < 5 ? "border-b-[1px] border-[#fff] grow-1 flex-grow" : "border-t-[1px] border-[#fff] grow-1 flex-grow"}></div>
+                    <div className={props.index < 5 ? "border-t-[1px] border-[#FFF7] grow-1 flex-grow" : "border-b-[1px] border-[#FFF7] grow-1 flex-grow"}></div>
+                        <div className={props.index < 5 ? "border-l-[1px] border-[#FFF7] w-[30px]" : "border-l-[1px] border-[#FFF7] w-[30px]"} style={props.index < 5 ? {transform: "skewX(45deg)", marginLeft: '15px',marginRight: '-15px'} : {transform: "skewX(-45deg)", marginLeft: '15px',marginRight: '-15px'}}></div>
+                        <div className={props.index < 5 ? "border-b-[1px] border-[#FFF7] grow-1 flex-grow" : "border-t-[1px] border-[#FFF7] grow-1 flex-grow"}></div>
                     </div>
                 </div>
             );
